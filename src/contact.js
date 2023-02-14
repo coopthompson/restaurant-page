9dfe0e32f6a6672c6d164a4f01b0dfa13d42257c
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-02-14 01:48:37 -0500

    Add more style to all pages

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,16 @@
+import './call.css'
+
 function contactHeader () {
     const headerFull = document.createElement('h1');
     const headerText = document.createTextNode('Contact Us');
     headerFull.appendChild(headerText);
+    headerFull.classList.add('header-style')
     return headerFull;
 };
 
 function contactProfile () {
     const explainBox = document.createElement('div');
+    const textBox = document.createElement('div');
     const chefName = document.createElement('h3');
     const nameText = document.createTextNode('Skittlzeby Jones');
     chefName.appendChild(nameText);
@@ -17,11 +21,15 @@ function contactProfile () {
     const profileText = document.createTextNode('Skittlzeby has been working as a chef in the industry for 4 weeks now! He is soooo talented! Call to reserve a table');
     const chefImage = document.createElement('img');
     chefImage.src = '../skittlezby.png'
+    chefImage.classList.add('pic-style')
     explainProfile.appendChild(profileText);
-    explainBox.appendChild(chefName);
-    explainBox.appendChild(phoneNumber);
-    explainBox.append(explainProfile);
+    textBox.appendChild(chefName);
+    textBox.appendChild(phoneNumber);
+    textBox.append(explainProfile);
+    textBox.classList.add('text-style');
     explainBox.append(chefImage);
+    explainBox.append(textBox);
+    explainBox.classList.add('contact-style');
 
     return explainBox;
 };
--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,28 @@
+import './choose.css'
+
 const menuItem = (name, price, desc, image) => {
     const fullItem = document.createElement('div');
     const nameItem = document.createElement('h2');
-    const priceItem = document.createElement('p');
+    const priceItem = document.createElement('h2');
     const descItem = document.createElement('p');
     const nameText = document.createTextNode(name);
     const priceText = document.createTextNode(price);
     const descText = document.createTextNode(desc);
     const itemImage = document.createElement('img');
+    const namePrice = document.createElement('div')
     itemImage.src = image
+    fullItem.classList.add('menu-style')
     nameItem.appendChild(nameText);
     priceItem.appendChild(priceText);
+    namePrice.appendChild(nameItem);
+    namePrice.appendChild(priceItem);
+    namePrice.classList.add('item-style')
     descItem.appendChild(descText);
-    fullItem.appendChild(nameItem);
-    fullItem.appendChild(priceItem);
+    descItem.classList.add('desc-style')
+    fullItem.appendChild(namePrice);
     fullItem.appendChild(descItem); 
     fullItem.appendChild(itemImage);
+    itemImage.classList.add('pic-style')
     return fullItem;
 }
 
